0e77e291edd4916d6fdbccf105ec92ff1ceafd61
Ramon Saviato <ramonsaviato@hotmail.com>
2025-10-08 20:35:36 -0300

    add Customer/State core entities

---
+++ core/src/main/java/com/ramon/loans/core/entities/Customer.java
@@ -0,0 +1,26 @@
+package com.ramon.loans.core.entities;
+
+import java.math.BigDecimal;
+
+import com.ramon.loans.core.enums.State;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    private String name;
+    private String cpf;
+    private Integer age;
+    private BigDecimal income;
+    private State location;
+
+    public Customer(String name, String cpf, Integer age, BigDecimal income, String location) {
+        this(name, cpf, age, income, State.valueOf(location));
+    }
+
+}
