30e608e4ad21647ab972120aad76e4bce1c90f5b
Ramon Saviato <ramonsaviato@hotmail.com>
2025-10-08 21:32:26 -0300

    add DisjunctionSpecification spec

--- core/src/main/java/com/ramon/loans/core/specifications/base/DisjunctionSpecification.java
+++ core/src/main/java/com/ramon/loans/core/specifications/base/DisjunctionSpecification.java
@@ -3,14 +3,14 @@ package com.ramon.loans.core.specifications.base;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ConjunctionSpecification<T> implements BaseSpecification<T> {
+public class DisjunctionSpecification<T> implements BaseSpecification<T> {
 
     private final BaseSpecification<T> leftSpec;
     private final BaseSpecification<T> rightSpec;
 
     @Override
     public boolean isSatisfiedBy(T candidate) {
-        return leftSpec.isSatisfiedBy(candidate) && rightSpec.isSatisfiedBy(candidate);
+        return leftSpec.isSatisfiedBy(candidate) || rightSpec.isSatisfiedBy(candidate);
     }
 
 }
