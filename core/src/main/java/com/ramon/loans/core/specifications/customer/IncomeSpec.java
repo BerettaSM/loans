cda403c346382b903f6c860651bcc67ba8baa38c
Ramon Saviato <ramonsaviato@hotmail.com>
2025-10-10 19:25:10 -0300

    add IncomeSpec for customers

---
+++ core/src/main/java/com/ramon/loans/core/specifications/customer/IncomeSpec.java
@@ -0,0 +1,32 @@
+package com.ramon.loans.core.specifications.customer;
+
+import java.math.BigDecimal;
+
+import com.ramon.loans.core.entities.Customer;
+import com.ramon.loans.core.specifications.base.Specification;
+
+public final class IncomeSpec {
+
+    public static final Specification<Customer> incomeLowerThan(Double income) {
+        return customer -> customer.getIncome().compareTo(BigDecimal.valueOf(income)) < 0;
+    }
+
+    public static final Specification<Customer> incomeLowerThanOrEqual(Double income) {
+        return customer -> customer.getIncome().compareTo(BigDecimal.valueOf(income)) <= 0;
+    }
+
+    public static final Specification<Customer> incomeHigherThan(Double income) {
+        return customer -> customer.getIncome().compareTo(BigDecimal.valueOf(income)) > 0;
+    }
+
+    public static final Specification<Customer> incomeHigherThanOrEqual(Double income) {
+        return customer -> customer.getIncome().compareTo(BigDecimal.valueOf(income)) >= 0;
+    }
+
+    public static final Specification<Customer> incomeBetween(
+            Double lowerBoundIncome,
+            Double upperBoundIncome) {
+        return incomeHigherThanOrEqual(lowerBoundIncome).and(incomeLowerThanOrEqual(upperBoundIncome));
+    }
+
+}
