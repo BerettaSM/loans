f92ee1339af3f4f75a943148f36e4ddb3b69e415
Ramon Saviato <ramonsaviato@hotmail.com>
2025-10-10 19:41:09 -0300

    add LocationSpec for customers

---
+++ core/src/main/java/com/ramon/loans/core/specifications/customer/LocationSpec.java
@@ -0,0 +1,16 @@
+package com.ramon.loans.core.specifications.customer;
+
+import java.util.Arrays;
+
+import com.ramon.loans.core.entities.Customer;
+import com.ramon.loans.core.enums.State;
+import com.ramon.loans.core.specifications.base.Specification;
+
+public final class LocationSpec {
+    
+    public static final Specification<Customer> locatedAt(State... locations) {
+        return customer -> Arrays.stream(locations)
+            .anyMatch(loc -> loc.equals(customer.getLocation()));
+    }
+
+}
