1573259eeafba7388ef88efbd2bbbb42d060db1c
Ramon Saviato <ramonsaviato@hotmail.com>
2025-10-23 21:40:07 -0300

    add GuaranteedLoanRule tests

--- application/src/test/java/com/ramon/loans/application/factories/LoanRuleFactory.java
+++ application/src/test/java/com/ramon/loans/application/factories/LoanRuleFactory.java
@@ -1,11 +1,16 @@
 package com.ramon.loans.application.factories;
 
+import com.ramon.loans.application.ruleconfig.GuaranteedLoanRule;
 import com.ramon.loans.application.ruleconfig.PersonalLoanRule;
 
 public class LoanRuleFactory {
     
     public static final PersonalLoanRule createPersonalLoanRule() {
         return new PersonalLoanRule();
+    }
+
+    public static final GuaranteedLoanRule createGuaranteedLoanRule() {
+        return new GuaranteedLoanRule();
     }
 
 }
